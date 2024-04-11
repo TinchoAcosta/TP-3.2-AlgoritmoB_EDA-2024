65ede63f99fd64663f3725bd245b1fb4631b36c5
TinchoAcosta <soymartinacosta@hotmail.es>
2024-04-11 19:30:23 -0300

    Algoritmo b. subido

--- src/tp/pkg3/pkg2/EquilibrioDeParentesis.java
+++ src/tp/pkg3/pkg2/EquilibrioDeParentesis.java
@@ -2,30 +2,47 @@ package tp.pkg3.pkg2;
 
 public class EquilibrioDeParentesis {
     public static void validar(String cadena, Pila pila){
-        char aux;
         
         for(char caracter:cadena.toCharArray()){
-            if(caracter=='>' || caracter=='<' 
-               || caracter=='(' || caracter==')'
-               || caracter=='{' || caracter=='}'
-               || caracter=='[' || caracter==']'){
+            if(validacionDeApertura(caracter)){
                 pila.push(caracter);
-            }
-        }
-        
-        if(pila.verElemento()!='>' & 
-           pila.verElemento()!=')' &
-           pila.verElemento()!=']' &
-           pila.verElemento()!='}'){
-            System.out.println("Parentesis irregulares");
-        }
-        
-        while(!pila.pilaVacia()){
-            aux = pila.verElemento();
-            pila.pop();
-            if(aux==ParentesisDeCierre.CORCHETE.getSimbolo()){
                 
+            }else if(pila.pilaVacia()){
+                System.out.println("Parentesis irregulares.");
+                return;
+            }else if((pila.verElemento()=='(' && caracter==')') || 
+                    (pila.verElemento()=='[' && caracter==']') ||
+                    (pila.verElemento()=='{' && caracter=='}') ||
+                    (pila.verElemento()=='<' && caracter=='>')){
+                pila.pop();
+            }else if(validacionTipoCaracter(caracter)){
+                System.out.println("Parentesis irregulares.");
+                return;                
             }
-        }           
+        }
+        
+        System.out.println("Parentesis son regulares.");
+    }
+    
+    private static boolean validacionDeApertura(char aux){
+        if (aux=='<' || 
+           aux=='(' ||
+           aux=='{' ||
+           aux=='['){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    private static boolean validacionTipoCaracter(char aux){
+        if(aux=='(' || aux==')' ||
+           aux=='[' || aux==']' ||
+           aux=='{' || aux=='}' ||
+           aux=='<' || aux=='>'){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
--- src/tp/pkg3/pkg2/Main.java
+++ src/tp/pkg3/pkg2/Main.java
@@ -3,7 +3,9 @@ package tp.pkg3.pkg2;
 public class Main {
 
     public static void main(String[] args) {
-
+        Pila pila = new Pila();
+        String cad="[(])";
+        EquilibrioDeParentesis.validar(cad, pila);
     }
     
 }
